cd164fbec94cd22801d43e36e02bf68220a5174c
nadiahdella <64344049+nadiahdella@users.noreply.github.com>
2020-04-26 11:49:26 +0700

    Add files via upload

---
+++ PBO2/RetailItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBO2
+{
+    class RetailItem
+    {
+        private String description;
+        private int unitsOnHand;
+        private double price;
+
+        public void setDescription(String userDescription)
+        {
+            description = userDescription;
+        }
+
+        public void setUnitsOnHand(int userUnitsOnHand)
+        {
+            unitsOnHand = userUnitsOnHand;
+        }
+
+        public void setPrice(double userPrice)
+        {
+            price = userPrice;
+        }
+
+        public String getDescription()
+        {
+            return description;
+        }
+        public int getUnitsOnHand()
+        {
+            return unitsOnHand;
+        }
+
+        public Double getPrice()
+        {
+            return price;
+        }
+
+        public RetailItem(String descriptionGiven, int unitsOnHandGiven, double priceGiven)
+        {
+            description = descriptionGiven;
+            unitsOnHand = unitsOnHandGiven;
+            price = priceGiven;
+        }
+    }
+}
